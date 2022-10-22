e1ccd3751e063217097eaa25e58790c3b51cf84f
Alex Adams <70454645+aadams149@users.noreply.github.com>
2022-10-22 14:28:08 -0400

    Update

--- data_visualization_AAdams.R
+++ data_visualization_AAdams.R
@@ -9,12 +9,15 @@ require(dplyr)
 require(ggplot2)
 
 # Read in Data ----
+#List of years
 years <-
   c(2014:2020)
 
+#Instantiate new empty data frame
 data <-
   data.frame()
 
+#Loop over years, read in data
 for (year in years){
   filename <-
     paste0(
@@ -34,15 +37,19 @@ for (year in years){
     bind_rows(df)
 }
 
+#Remove unneeded object
 rm(df)
 
 # Read in workshop categories ----
+
+#Load in workshop categories
 categories <-
   readxl::read_xlsx(
     'Workshop Categories.xlsx',
     sheet = 'Workshop names'
   )
 
+#Join data to categories
 data <-
   data %>%
   left_join(
@@ -53,15 +60,20 @@ data <-
 # Create some data visualizations ----
 
 # Group # of attendees by category/year ----
+
+#Group data by workshop category and year
 data %>%
   group_by(
    `Workshop Category`,
    year
   ) %>%
+  #Count number of occurrences 
   summarise(
     count = n()
   ) %>%
+  #Ungroup
   ungroup() %>%
+  #Pipe data into bar chart
   ggplot(
     aes(
       x = factor(year),
@@ -69,7 +81,13 @@ data %>%
       fill = `Workshop Category`)) +
   geom_col(position = 'dodge') +
   theme_bw() +
+  #Fill colors with official SWT colors
+  scale_fill_manual(values = c('#af1f27',
+                               '#f1e21f',
+                               '#1a8c47')) +
+  #Make sure all labels are present
   theme(axis.text.x = element_text(angle = 60, vjust = 0.5)) +
+  #Add plot title and axis labels
   ggtitle('Number of Attendees at SWT Workshops by Category, 2014-2020') +
   ylab('Number of Attendees') +
   xlab('Year') +
@@ -122,25 +140,3 @@ data %>%
   xlab('Workshop Category') +
   labs(caption = 'Data Provided by Starting With Today')
 
-
-
-data %>%
-  group_by(
-    year
-  ) %>%
-  summarise(
-    count = n()
-  ) %>%
-  ungroup() %>%
-  ggplot(
-    aes(
-      x = factor(year),
-      y = count)) +
-  geom_col(position = 'dodge',
-           fill = '#1a8c47') +
-  theme_bw() +
-  theme(axis.text.x = element_text(angle = 60, vjust = 0.5)) +
-  ggtitle('Number of Attendees at SWT Workshops, 2014-2021') +
-  ylab('Number of Attendees') +
-  xlab('Year') +
-  labs(caption = 'Data Provided by Starting With Today')
